123970dbe10687e85da0ed22d5085c9bdf3fa922
Elijah Roussos <elijah.rou@gmail.com>
2023-10-29 22:51:07 +0200

    add tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,29 @@
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+  <div class="logo">
+    <h1 class="text-3xl font-bold underline">Dear Diary</h1>
+  </div>
+
+  <div>On May 4th, 2023 I Met You (Through goat sacrifice)</div>
+
+  <div>
+    From Sushi Making To Ping Pong And Movie Nights Or Central Park Picnics
+  </div>
+
+  <div>I fell in Love</div>
+
+  <div>We became explorers... roommates... lovers.. best friends</div>
+
+  <div>No matter Where you are or What we do</div>
+
+  <div>
+    I will always love your (bum....) smile... passion... (body...) black
+    hair... writing... heart... eyes... kindness... thoughtfulness...
+    tenacity... creativity... You.
+  </div>
+
+  <div>I love you My Darling Girl Cecelia</div>
 
   <div class="card">
     <Counter />
